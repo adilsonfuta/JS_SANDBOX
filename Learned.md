3019b1c49f6579b2a6b3269d45809af89ca2261b
Adilson Futa <adilson.futa@novadev.ao>
2024-02-18 22:58:38 +0100

    template literal

--- Learned.md
+++ Learned.md
@@ -187,4 +187,38 @@ console.log(result.toFixed(2));
 
 console.log(Math.floor(Math.random()*valor+1));
 
+--------  Learning Strings
+const firstName = 'Adilson';
+const lastName = 'Futa';
+const age = 36;
+let valor;
+
+valor = firstName + ' '+ lastName;
+console.log(valor);
+
+let appendStr = 'Novo';
+appendStr += ' Teste';
+
+appendStr = 'Hello, my name is '+firstName+ ' and am '+age;
+appendStr = " That 's awesome, i can't wait ";
+
+appendStr = firstName.length;
+appendStr = firstName.concat(' ',lastName);
+appendStr = appendStr.toUpperCase(appendStr);
+appendStr = appendStr.toLowerCase(appendStr);
+appendStr = firstName[0];
+appendStr = firstName.indexOf('l');
+appendStr = firstName.lastIndexOf('l');
+appendStr = firstName.charAt('5');
+appendStr = firstName.charAt(firstName.length-1);
+appendStr = firstName.substring(0,3);
+appendStr = firstName.slice(0,5);
+appendStr = firstName.slice(-3);
+
+const str = 'Hello my name is Adeler';
+appendStr = str.split(' ');
+appendStr = str.replace('Adeler','Jack');
+appendStr = str.includes('Hello');
+
+console.log(appendStr);
 
--- app.js
+++ app.js
@@ -1,35 +1,13 @@
-const firstName = 'Adilson';
-const lastName = 'Futa';
-const age = 36;
+// Learning Template Literal or / Template String  es5
 
-let valor;
+const nome = 'John';
+const age = 30;
+const job = 'web Developer';
+const city = 'Luanda';
 
-valor = firstName + ' '+ lastName;
+let html;
 
-console.log(valor);
+html = '<ul><li>Nome: '+ nome +'</li><li>Idade: '+age+'</li><li>Trabalho: '+job+'</li><li>Cidade: '+city+'</li></ul>';
 
-let appendStr = 'Novo';
-appendStr += ' Teste';
+document.body.innerHTML=html;
 
-appendStr = 'Hello, my name is '+firstName+ ' and am '+age;
-appendStr = " That 's awesome, i can't wait ";
-
-appendStr = firstName.length;
-appendStr = firstName.concat(' ',lastName);
-appendStr = appendStr.toUpperCase(appendStr);
-appendStr = appendStr.toLowerCase(appendStr);
-appendStr = firstName[0];
-appendStr = firstName.indexOf('l');
-appendStr = firstName.lastIndexOf('l');
-appendStr = firstName.charAt('5');
-appendStr = firstName.charAt(firstName.length-1);
-appendStr = firstName.substring(0,3);
-appendStr = firstName.slice(0,5);
-appendStr = firstName.slice(-3);
-
-const str = 'Hello my name is Adeler';
-appendStr = str.split(' ');
-appendStr = str.replace('Adeler','Jack');
-appendStr = str.includes('Hello');
-
-console.log(appendStr);
