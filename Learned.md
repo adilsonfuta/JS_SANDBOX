9f027d1718b3bee12a3dd4f4e7aa36d4eff02550
Adilson Futa <adilson.futa@novadev.ao>
2024-02-19 22:47:17 +0100

    object literal

--- Learned.md
+++ Learned.md
@@ -261,3 +261,48 @@ html = '<h1> Template Literal (ES5)</h1>'+
 
 document.body.innerHTML=html;
 
+--------
+
+const num = [43,47,8,9,12,3,4,6,788];
+const vect = new Array(2,3,4,5,88,1);
+const frutas = ['apple','banana','orange','pear'];
+const mixed = [22,'hello',true,undefined,null,num,{a:1,b:2},new Date()];
+
+let val;
+
+val = Array.isArray(num);
+num[3]=100;
+val = num[3];
+val = num.indexOf(788);
+num.push(2000);
+num.unshift(100);
+
+num.pop();
+num.shift();
+//num.splice(1,4);
+num.reverse();
+val = num.concat(mixed);
+val = frutas.sort();
+val = num.sort();
+
+val = num.sort(function (a,b) {
+  return a-b
+})
+
+const arranjo = [43,47,8,9,12,3,4,6,788];
+
+val = arranjo.find(function (n) {
+  return n<50;
+})
+
+function under50(params) {
+  return params<3;
+}
+function over50(params) {
+  return params>50;
+}
+val = arranjo.find(over50);
+
+console.log(val);
+//console.log(num);
+
--- app.js
+++ app.js
@@ -1,42 +1,50 @@
-const num = [43,47,8,9,12,3,4,6,788];
-const vect = new Array(2,3,4,5,88,1);
-const frutas = ['apple','banana','orange','pear'];
-const mixed = [22,'hello',true,undefined,null,num,{a:1,b:2},new Date()];
+// Object lITERals
+var anoAtual = 2024;
+
+const person = {
+  firstName: 'Steve',
+  lastName: 'Jonhson',
+  age: 32,
+  email: 'steve@novadev.ao',
+  hobbies: ['music','technology','patins','viagem','explorar'],
+  address: {
+    city: 'Miami',
+    state: 'FL'
+  },
+   getAniversario : function () {
+    //return 2024 - person.age;
+    return anoAtual - this.age;
+   } 
+}
 
 let val;
 
-val = Array.isArray(num);
-num[3]=100;
-val = num[3];
-val = num.indexOf(788);
-num.push(2000);
-num.unshift(100);
-
-num.pop();
-num.shift();
-//num.splice(1,4);
-num.reverse();
-val = num.concat(mixed);
-val = frutas.sort();
-val = num.sort();
-
-val = num.sort(function (a,b) {
-  return a-b
-})
-
-const arranjo = [43,47,8,9,12,3,4,6,788];
-
-val = arranjo.find(function (n) {
-  return n<50;
-})
-
-function under50(params) {
-  return params<3;
-}
-function over50(params) {
-  return params>50;
-}
-val = arranjo.find(over50);
+val = person;
+val = person.firstName;
+val = person['firstName'];
+val = person['age']
+val = person.hobbies[3];
+val = person.address.city;
+val = person.address['state'];
+val = person['address']['city'];
+val = person.getAniversario();
 
 console.log(val);
-//console.log(num);
+
+const people = [
+  {
+    city: 'Miami',
+    state: 'FL'
+  },
+  {nome: 'Jhon', age: 30},
+  {nome: 'PEDRO', age: 32},
+  {nome: 'Beto', age: 44},
+  {nome: 'Vadinho', age: 50}
+ 
+];
+
+const tam = people.length;
+for (let i = 0; i < tam; i++) {
+  //const element = people[i].nome;
+  console.log(people[i].nome);
+}
