059da7066299a85bd3257326b1555762a34e689c
Adilson Futa <adilson.futa@novadev.ao>
2024-02-21 00:49:00 +0100

    trabalhando com functions js

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 
-console.clear();
 let val;
+// functions declarations 
 function greet(firstName = 'BTS', lastName = 'Band'){
   return `Your name is ${firstName} ${lastName}`;
 }
 
 val = greet('adilson','Smith');
 
+// functions expression 
 const squareValues = function (n) {
   return Math.pow(n,2);
 }
 
 val = squareValues(8);
 
+// IIFES - IMEDIATILY INVOKE FUNCT EXPRSSION 
+// elas funcionam como metodos do algoritmos pois nao retorna nada.
+
+(function(){
+
+})();
+
+
+(function(){
+  console.log(' testando IIFES sem argumentos');
+})();
+
+(function(pais = 'Angola'){
+      console.log(`O pais chama-se ${pais}`);
+})('Brazil')
+
+console.clear();
+
+const todo = {
+  add: function(){
+      console.log(' ADD crud');
+  },
+  edit: function(id = null){
+      console.log(`Edit ID values ${id}`);
+  }
+}
+
+val = todo.add();
+val = todo.edit(23);
+
+todo.delete = function (){
+    console.log('Delete element CRUD');
+};
+
+val = todo.delete();
+
 console.log(val);
+console.log(todo);
